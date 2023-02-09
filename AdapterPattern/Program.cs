232a0497f636db3755d7f5526051fb231df65a17
Pham Tuan Hiep <phamtuanhiephs@gmail.com>
2023-02-09 17:00:29 +0700

    first commit

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ishape> shapes = new List<Ishape>() { new LineAdapter(new LegacyLine()), new RectangleAdapter(new LegacyRectangle()) };
+            int x1 = 5, y1 = 6, x2 =-3 , y2 = -2;
+            shapes.ForEach(shape => shape.Draw(x1, y1, x2, y2));
+        }
+    }
+    internal interface Ishape
+    {
+        void Draw(int x1, int y1, int x2, int y2);
+    }
+    #region Adapter
+    internal class RectangleAdapter:Ishape
+    {
+        private readonly LegacyRectangle _legacyRectangle;
+        public  RectangleAdapter(LegacyRectangle legacyRectangle)
+        {
+            _legacyRectangle = legacyRectangle;
+        }
+        public void Draw(int x1,int y1,int x2,int y2)
+        {
+            int x = Math.Min(x1, x2);
+            int y = Math.Min(y1, y2);
+            int w = Math.Abs(x2 - x1);
+            int h = Math.Abs(y2 - y1);
+            _legacyRectangle.Draw(x, y, w, h);
+        }
+    }
+    internal class LineAdapter:Ishape
+    {
+        private readonly LegacyLine _legacyLine;
+        public LineAdapter(LegacyLine legacyLine)
+        {
+            _legacyLine = legacyLine;
+        }
+        public void Draw(int x1, int y1, int x2, int y2)
+        {
+            _legacyLine.Draw(x1, y1,x2,y2);
+        }
+    }
+    #endregion
+    #region Legacy
+    internal class LegacyRectangle:Ishape
+    {
+            public void Draw(int x,int y,int w,int h)
+            {
+                Console.WriteLine($"Draw Rectangle {x} {y} {w} {h}");
+            }
+    }
+    internal class LegacyLine:Ishape
+    {  
+            public  void Draw(int x1, int y1, int x2, int y2)
+            { 
+                Console.WriteLine("Draw Line {0} {1} {2} {3}",x1,y1,x2,y2);
+            }
+
+    }
+    #endregion
+}
---
+++ AdapterPattern/RectangleAndTriangle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    #region Interface
+    public interface IRectangle
+    {
+        void AboutRectangle();
+        double CalculateAreaOfRectangle();
+    }
+    public interface ITriangle
+    {
+        void AboutTriangle();
+        double CalculateAreaOfTriangle();
+    }
+    #endregion
+    #region Rect and Tria
+    public class Rectangle:IRectangle
+    {
+        public double length;
+        public double width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double CalculateAreaOfRectangle()
+        {
+            return length * width;
+        }
+        public void AboutRectangle()
+        {
+            Console.WriteLine("This is Rectangle");
+        }
+    }
+    public class Triangle : ITriangle
+    {
+        public double hypotenues;
+        public double height;
+        public Triangle(double b, double h)
+        {
+            hypotenues = b;
+            height = h;
+        }
+        public double CalculateAreaOfTriangle()
+        {
+            return 0.5 * hypotenues * height;
+        }
+        public void AboutTriangle()
+        {
+            Console.WriteLine("This is Triangle");
+        }
+    }
+    #endregion
+    public class TriangleAdapter : IRectangle
+    {
+        Triangle triangle;
+        public TriangleAdapter(Triangle t)
+        {
+            this.triangle = t;
+        }
+        public double CalculateAreaOfRectangle()
+        {
+             return triangle.CalculateAreaOfTriangle();
+        }
+        public void AboutRectangle()
+        {
+           triangle.AboutTriangle();
+        }
+    }
+    class Start
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Adapter Design Pattern:");
+
+            Rectangle r = new Rectangle(20, 10);
+            Console.WriteLine("\n Area of Rectangle is {0} square unit", r.CalculateAreaOfRectangle());
+
+            Triangle t = new Triangle(20,10);
+            Console.WriteLine("\n Area of Triangle is {0} square unit \n", t.CalculateAreaOfTriangle());
+
+            IRectangle adapter = new TriangleAdapter(t);
+            Console.WriteLine(" Area of Triangle is {0} square unit", GetArea(adapter));
+            double GetArea(IRectangle ir)
+            {
+                ir.AboutRectangle();
+                return ir.CalculateAreaOfRectangle();
+            }
+            Console.ReadLine();
+        }
+    }
+}
